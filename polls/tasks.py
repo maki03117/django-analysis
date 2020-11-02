850cbeb5ecd183a00e7307bf1ae172f75067bf16
maki03117 <32335810+maki03117@users.noreply.github.com>
2020-11-02 06:48:08 +0000

    changes to show % of profits

--- polls/tasks.py
+++ polls/tasks.py
@@ -52,7 +52,7 @@ def gen_analyse(arr, group):
   arr.append(total_deposits)
   
   # DEPOSITS %
-  deposite_rate = round((num_deposits / total_records) * 100) 
+  deposite_rate = round((total_deposits / total_value) * 100) 
   arr.append(str(deposite_rate)+"%")
 
   return 
